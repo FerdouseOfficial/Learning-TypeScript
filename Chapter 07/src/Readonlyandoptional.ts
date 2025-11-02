ca1e24c785e5174dda39b45179300a90fb9dc71f
FerdouseOfficial <mollajihad402@gmail.com>
2025-11-02 18:36:36 +0600

    I am practicing the conecpt of readonly and ? keyword

--- Chapter 07/src/Readonlyandoptional.ts
+++ Chapter 07/src/Readonlyandoptional.ts
@@ -5,3 +5,13 @@
             // 2. the second one is  just a question mark? literal question mark.
             // 3. third one is a use case scenario that you'll be usin ga lot.
 
+
+
+// here we are learning the READONLY keyword and the "?" question mark keyword as optional
+
+// here is the example. okay.
+
+type User = {
+    readonly _id: number,
+    
+}
