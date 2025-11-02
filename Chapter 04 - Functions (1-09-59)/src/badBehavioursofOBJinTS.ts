fd0c31e5b81c111bd04b2bdbffc887e65b179a9d
FerdouseOfficial <mollajihad402@gmail.com>
2025-11-02 12:01:42 +0600

    writing very important code follow along with tutorial. And also I have learn a new thing name destructuring in JavaScript okay. That was supercool way. and I have write so many comment make sure to read every comment every coment is so much important after reading that if you don't understand someting ask GPT okay. But don't avoid them

---
+++ Chapter 04 - Functions (1-09-59)/src/badBehavioursofOBJinTS.ts
@@ -0,0 +1,27 @@
+// her we will learn more about bad behaviour of object in TypeScript okay.
+const User = {
+    name: "Rohan",
+    email: "Rohan32@com",
+    isActive: true
+}
+
+// in up how i write the object actually this is not a stadard way of writing object in TS.
+
+// function createUser({name: string, isPaid: boolean}){} this is not the right way of writing syntax .
+// here is the right syntax here
+function createUser ({name, isPaid}: {name: string, isPaid: boolean}){} // this is not the actual way of declaring but this is how you do it inside the funciton okay.
+
+let newUser = {name: "ali", isPaid: true, email: "ali@gmail.com"} // "This is how" we can do that now when we pass the newUser into the create user it doesn't show any error. Thaat's the bad behaviour but ther is way to fix it. We will learn it later by type and interfeaces okay. Remember it .
+
+createUser({name: "ROhan", isPaid: true}) // if we want now we can't define any more values like email because it is not defined in the function createUser. but we can do that here is how it will show in the above section with "this is how"
+
+// Practicing Destructuring here is the link of the chat if you have any further confuseion go here and learn about it okay.
+// here is the link:- https://chatgpt.com/share/6906eedd-b914-800f-a9fb-170e305955a9
+
+// const alpha = ['a', 'b', 'd', 3];
+// const [f1, f2, , f3] = alpha; // This is how destrucing works okay.
+
+// console.log(f1)
+
+// in one line
+// Destructuring is :- A quick way to unpack values from arrays or objects into separate variables. that's it okay.
