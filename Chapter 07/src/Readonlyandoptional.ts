608fcbf359aa918b201e32d08c7e38c7809d0625
FerdouseOfficial <mollajihad402@gmail.com>
2025-11-02 13:00:35 +0600

    writing the conept we will learn in this chapter

--- Chapter 07/src/Readonlyandoptional.ts
+++ Chapter 07/src/Readonlyandoptional.ts
@@ -1,1 +1,7 @@
 // her we will practiceth READONLY and optional okay.
+
+// her we will learn 3 simple things
+            // 1. The fist one is a keyword wich says read only
+            // 2. the second one is  just a question mark? literal question mark.
+            // 3. third one is a use case scenario that you'll be usin ga lot.
+
