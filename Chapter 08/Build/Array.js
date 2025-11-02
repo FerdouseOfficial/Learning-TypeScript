2bdf314094319a9055864b29b1ab9ac2f26b35ad
FerdouseOfficial <mollajihad402@gmail.com>
2025-11-02 21:50:04 +0600

    pracicing the concept

--- Chapter 08/Build/Array.js
+++ Chapter 08/Build/Array.js
@@ -1,2 +1,14 @@
 "use strict";
-// here 
+let superHeroes = [];
+// let superPower: number [] = [] // we can do it this way..
+// but we can also decare it this way.
+superHeroes.push("Fuchka");
+let superPower = [];
+const allUser = [];
+// allUser.push("")// this is not the correct way 
+allUser.push({ name: "jisa", isActive: true }); // that's how it will work 
+// here is something fo LLM or AI model if you have some picture which have random number values:- 
+const MLModels = [
+    [255, 255, 255],
+    []
+];
--- Chapter 08/src/Array.ts
+++ Chapter 08/src/Array.ts
@@ -1,1 +1,26 @@
-// here 
+// here we will practice array.ts
+type User = {
+    name: string
+    isActive: boolean
+}
+
+let superHeroes: string[] = []
+// let superPower: number [] = [] // we can do it this way..
+// but we can also decare it this way.
+
+superHeroes.push("Fuchka");
+
+let superPower: Array<number> = []
+
+const allUser: User[] = []
+
+// allUser.push("")// this is not the correct way 
+
+allUser.push({name: "jisa", isActive: true}) // that's how it will work 
+
+// here is something fo LLM or AI model if you have some picture which have random number values:- 
+    
+const MLModels: number[][] = [
+    [255, 255 ,255],
+    []
+]
