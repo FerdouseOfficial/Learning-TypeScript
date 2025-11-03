90136a0dabfea9f2863590d57d9cd375ec99ec9e
FerdouseOfficial <mollajihad402@gmail.com>
2025-11-03 18:43:00 +0600

    in this chapter we don't really learn anything. Hitesh just teach only how to setup workflow to work which I have alreay learn from dave gray

--- Chapter 14/dist/SettingUPTSforREalProduction.js
+++ Chapter 14/dist/SettingUPTSforREalProduction.js
@@ -1,2 +1,4 @@
 "use strict";
 // Practicing the concepts of Real life uses of it........
+let body = document.querySelector("body");
+body.style.backgroundColor = "#000";
--- Chapter 14/src/SettingUPTSforREalProduction.ts
+++ Chapter 14/src/SettingUPTSforREalProduction.ts
@@ -1,3 +1,5 @@
 // Practicing the concepts of Real life uses of it........
 
- 
+let body = document.querySelector("body") as HTMLBodyElement ;
+
+body.style.backgroundColor = "#000";
