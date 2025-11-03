2abbae8c2ed2d67b77b60c19d9cc2d096d59c828
FerdouseOfficial <mollajihad402@gmail.com>
2025-11-03 16:56:50 +0600

    write the concept of hte Interface and its the finish of this 12 chapter

--- Chapter 12/Build/Interfaces.js
+++ Chapter 12/Build/Interfaces.js
@@ -5,7 +5,11 @@
 let hitesh = { dbId: 22, name: "Hc", id: 1,
     startTrial: () => {
         return "Fuck Off Man"; // here is how you do it man declare a method
+    }, getCoupon: (name, value) => {
+        return 3;
     }
 }; // that's the perfect way of doing it.
 hitesh.email = "WakaWakkaOOOO";
 // hitesh.dbId = 34 // we can do that because we cannot change or reassing readonly value remember it
+// the concept of Interface
+// Yur operating sytem is a interface for hardware it doesn't allow you that when you click a utton how does it happpen you don't know ad you don't care about it you just know ifI click on this something happens if I click on this. You don't know what deep down is happening. that's the simple theory 
--- Chapter 12/src/Interfaces.ts
+++ Chapter 12/src/Interfaces.ts
@@ -10,6 +10,7 @@ interface user {
     // startTrial: () => string 
     // anotehr way of doing int the defination of method which hitesh like is this.
     startTrial(): string // its also a way you can do that
+    getCoupon(couponName: string, value: number): number
 }
 
 // let hitesh= {name:"hitesh", id: 1}  // that's most beginer make mistake like me they forgot to assign the user interface 
@@ -19,6 +20,8 @@ interface user {
 let hitesh: user = {dbId: 22, name: "Hc", id: 1, 
     startTrial : () => {
         return "Fuck Off Man" // here is how you do it man declare a method
+    }, getCoupon:(name: "hitesh10", value: 3) => {
+        return 3
     }
 } // that's the perfect way of doing it.
 
@@ -26,3 +29,8 @@ hitesh.email = "WakaWakkaOOOO"
 
 // hitesh.dbId = 34 // we can do that because we cannot change or reassing readonly value remember it
 
+
+
+// the concept of Interface
+
+// Yur operating sytem is a interface for hardware it doesn't allow you that when you click a utton how does it happpen you don't know ad you don't care about it you just know ifI click on this something happens if I click on this. You don't know what deep down is happening. that's the simple theory 
