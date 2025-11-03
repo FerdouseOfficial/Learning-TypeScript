1df1bb6a67c95e3a8b062f9fea61a61575e21dbb
FerdouseOfficial <mollajihad402@gmail.com>
2025-11-03 16:19:36 +0600

    I have write all the important code and notes needed for you to understand the concepts fo Enums

--- Chapter 11/Build/Enums.js
+++ Chapter 11/Build/Enums.js
@@ -1,1 +1,27 @@
 "use strict";
+// Practicing concepts of Enum in TS 
+// enum SeatChoice {
+//     AISLE,
+//     MIDDLE,
+//     WINDOW,
+//     FOURTH
+// }
+// enum SeatChoice { /// YOu also can set there specific value on them just like that that's will be fine okay
+//     AISLE = 10 ,
+//     MIDDLE= 12,
+//     WINDOW = 202,
+//     FOURTH
+// }
+var SeatChoice;
+(function (SeatChoice) {
+    SeatChoice["AISLE"] = "aisle";
+    SeatChoice["MIDDLE"] = "middle";
+    SeatChoice["WINDOW"] = "window";
+    SeatChoice["FOURTH"] = "fourth";
+})(SeatChoice || (SeatChoice = {}));
+// there is a lot of way you can do tha
+// HERE IS HOW YOU WILL ACCESS THEM :- 
+let hcSeat = SeatChoice.AISLE; // tha'ts how
+// when You see the compiled version of it its a very huge function so to make it small add "CONST" infront of te enum okay
+// REMEMBER
+// Teh moto behind having a typescript is having a restricton while writing the code but the code that is being produced by the Javascrpt shold bbe clean and beautiful and that is why there's a lot of discussion and helathy discussion around the community which is good .
---
+++ Chapter 11/src/Enums.ts
@@ -0,0 +1,37 @@
+// Practicing concepts of Enum in TS 
+
+// enum SeatChoice {
+//     AISLE,
+//     MIDDLE,
+//     WINDOW,
+//     FOURTH
+// }
+
+
+// enum SeatChoice { /// YOu also can set there specific value on them just like that that's will be fine okay
+//     AISLE = 10 ,
+//     MIDDLE= 12,
+//     WINDOW = 202,
+//     FOURTH
+// }
+
+const enum SeatChoice {
+    AISLE = "aisle",
+    MIDDLE= "middle",              /////////// You also can do it just like this okay Hurrah this is so cool
+    WINDOW= "window", 
+    FOURTH= "fourth" 
+}
+ 
+
+// there is a lot of way you can do tha
+
+// HERE IS HOW YOU WILL ACCESS THEM :- 
+
+let hcSeat = SeatChoice.AISLE // tha'ts how
+
+
+// when You see the compiled version of it its a very huge function so to make it small add "CONST" infront of te enum okay
+
+// REMEMBER
+
+// Teh moto behind having a typescript is having a restricton while writing the code but the code that is being produced by the Javascrpt shold bbe clean and beautiful and that is why there's a lot of discussion and helathy discussion around the community which is good .
