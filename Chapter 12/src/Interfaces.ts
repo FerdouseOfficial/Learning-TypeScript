5cdacd941fdbc43d3f76af4f612bebd343b0948a
FerdouseOfficial <mollajihad402@gmail.com>
2025-11-03 16:42:13 +0600

    learning multiple way to write method inside interfaces

--- Chapter 12/src/Interfaces.ts
+++ Chapter 12/src/Interfaces.ts
@@ -6,14 +6,23 @@ interface user {
     email?: string,
     id: number,
     googleId?: string
+    // defining a method:-
+    // startTrial: () => string 
+    // anotehr way of doing int the defination of method which hitesh like is this.
+    startTrial(): string // its also a way you can do that
 }
 
 // let hitesh= {name:"hitesh", id: 1}  // that's most beginer make mistake like me they forgot to assign the user interface 
 
 /// here is how you do that
 
-let hitesh: user = {dbId: 22, name: "Hc", id: 1} // that's the perfect way of doing it.
+let hitesh: user = {dbId: 22, name: "Hc", id: 1, 
+    startTrial : () => {
+        return "Fuck Off Man" // here is how you do it man declare a method
+    }
+} // that's the perfect way of doing it.
 
 hitesh.email = "WakaWakkaOOOO"
 
 // hitesh.dbId = 34 // we can do that because we cannot change or reassing readonly value remember it
+
