6d25c3215af1538833ced4237ca3dad517b83cf3
FerdouseOfficial <mollajihad402@gmail.com>
2025-11-03 23:20:11 +0600

    writing so much code today

--- Chapter 17/Build/Getters&Setters.js
+++ Chapter 17/Build/Getters&Setters.js
@@ -10,27 +10,58 @@
 //         this.name = name;
 //     }
 // }
+let body = document.querySelector("body");
+body.style.backgroundColor = "#000";
 // here is the professional way of writing code. How the professional wriet code
-class User {
-    email;
-    name;
-    _courseCount = 1;
-    city = "Jaipur";
-    constructor(email, name) {
-        this.email = email;
-        this.name = name;
-    }
-    get getAppleEmail() {
-        return `apple ${this.email}`;
-    }
-}
+// class User{
+//     private _courseCount = 1;  
+//     private readonly city: string = "Jaipur"
+//     constructor(
+//         public email: string,
+//         public name: string) {
+//     }
+//     get getAppleEmail(): string {
+//         return `apple ${this.email}`;
+//     }
+// } 
 // this is the shortcut way of doing it.......
-const hitesh = new User("h@ghs", "hitesh");
+// const hitesh = new User("h@ghs", "hitesh")
 // hitesh.city = "delhi"
 // we have learned TypeScript PUBLIC AND PRIVATE:-  
 // Programming with MOsh getter and setter tutorial. Okay go ahead...
-const person = {
-    firstName: "Mosh",
-    lastName: "Hamedani"
-};
-console.log(`${person}`);
+// interface User {
+//     firstName: string,
+//     lastName: string,
+//     fullName(): string
+// }
+// const person: User = {
+//     firstName: "Mosh",
+//     lastName: "Hamedani",
+//     fullName: () => {
+//         return `${person.firstName} ${person.lastName}`
+//     }
+// };
+// console.log(person.fullName());
+// here is the very simple explnation of getter and setter
+//  f Very Simple Explanation:
+// get → used to get a value like a property.
+// → You don’t call it with ().
+// set → used to set or update a value.
+// → Runs automatically when you assign something.
+// this is the JavaScript version of Getters and Setter. 
+// const person = {
+//     firstName: "Mosh",
+//     lastName: "Hamedani",
+//     get fullName(){
+//         return `${person.firstName} ${person.lastName}`;
+//     },
+//     set fullName(value) {
+//         let parts = value.split(" ")
+//         value.firstName = parts[0];
+//         value.lastName = parts[1];
+//     }
+// }
+// let a = person.fullName = "Moash Hamedani"
+// console.log(a)
+// let b = person.fullName = "Joe Rogan"
+// console.log(b)
--- Chapter 17/src/Getters&Setters.ts
+++ Chapter 17/src/Getters&Setters.ts
@@ -12,27 +12,29 @@
 //     }
 // }
 
+let body = document.querySelector("body") as HTMLBodyElement;
+body.style.backgroundColor = "#000"
 // here is the professional way of writing code. How the professional wriet code
 
-class User{
-    private _courseCount = 1;  
+// class User{
+//     private _courseCount = 1;  
 
-    private readonly city: string = "Jaipur"
-    constructor(
-        public email: string,
-        public name: string) {
-    }
-    get getAppleEmail(): string {
-        return `apple ${this.email}`;
-    }
-} 
+//     private readonly city: string = "Jaipur"
+//     constructor(
+//         public email: string,
+//         public name: string) {
+//     }
+//     get getAppleEmail(): string {
+//         return `apple ${this.email}`;
+//     }
+// } 
 
 
 
 // this is the shortcut way of doing it.......
 
 
-const hitesh = new User("h@ghs", "hitesh")
+// const hitesh = new User("h@ghs", "hitesh")
 // hitesh.city = "delhi"
 
 // we have learned TypeScript PUBLIC AND PRIVATE:-  
@@ -43,11 +45,50 @@ const hitesh = new User("h@ghs", "hitesh")
 
 
 // Programming with MOsh getter and setter tutorial. Okay go ahead...
+// interface User {
+//     firstName: string,
+//     lastName: string,
+//     fullName(): string
+// }
 
-const person = {
-    firstName: "Mosh",
-    lastName: "Hamedani"
-};
+// const person: User = {
+//     firstName: "Mosh",
+//     lastName: "Hamedani",
+//     fullName: () => {
+//         return `${person.firstName} ${person.lastName}`
+//     }
+// };
 
-console.log(`${person}`);
+// console.log(person.fullName());
 
+// here is the very simple explnation of getter and setter
+
+//  f Very Simple Explanation:
+
+// get → used to get a value like a property.
+// → You don’t call it with ().
+
+// set → used to set or update a value.
+// → Runs automatically when you assign something.
+
+
+// this is the JavaScript version of Getters and Setter. 
+
+// const person = {
+//     firstName: "Mosh",
+//     lastName: "Hamedani",
+//     get fullName(){
+//         return `${person.firstName} ${person.lastName}`;
+//     },
+//     set fullName(value) {
+//         let parts = value.split(" ")
+//         value.firstName = parts[0];
+//         value.lastName = parts[1];
+//     }
+// }
+
+// let a = person.fullName = "Moash Hamedani"
+// console.log(a)
+
+// let b = person.fullName = "Joe Rogan"
+// console.log(b)
