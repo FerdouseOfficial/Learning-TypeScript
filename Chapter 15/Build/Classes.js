42af43c5ddaac021e5c8d3a537377a7104549954
FerdouseOfficial <mollajihad402@gmail.com>
2025-11-03 18:57:49 +0600

    learning how to write class in TS

--- Chapter 15/Build/Classes.js
+++ Chapter 15/Build/Classes.js
@@ -1,2 +1,18 @@
 "use strict";
 // Classes in TS . we will practice that.
+// class user {
+//     constructor(email: string, name: string){
+//         this.email = email;
+//         this.name = name;
+//     }
+// }
+// this is not the right sytax to write in TS. in Upper.
+// this is the right syntax for doing it. I think you understand.
+class user {
+    email;
+    name;
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
+    }
+}
--- Chapter 15/src/Classes.ts
+++ Chapter 15/src/Classes.ts
@@ -1,1 +1,21 @@
 // Classes in TS . we will practice that.
+
+// class user {
+//     constructor(email: string, name: string){
+//         this.email = email;
+//         this.name = name;
+//     }
+// }
+
+// this is not the right sytax to write in TS. in Upper.
+
+
+// this is the right syntax for doing it. I think you understand.
+class user {
+    email: string
+    name: string
+    constructor(email: string, name: string) {
+        this.email = email;
+        this.name = name
+    }
+}
