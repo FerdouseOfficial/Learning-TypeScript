1c9a9a72601e4719f204daff0f89575ba24c214b
FerdouseOfficial <mollajihad402@gmail.com>
2025-11-03 15:18:07 +0600

    writing important code and to understand the code you must have to read the comment of them

--- Chapter 09/src/UnionTypeInTS.ts
+++ Chapter 09/src/UnionTypeInTS.ts
@@ -19,3 +19,25 @@ function getDbId(id : number | string) {
         id.toUpperCase()
     }
 }
+
+let data: number[] = [1, 2, 3];
+let data2: string[] = ["1", "2", "3"];
+// let data3: number[] | string[] = ["1", 2, "3"] // by applying this syntax you cannot write 2 data type. You can only pass the value all string o rall will be number
+
+// here is the right way of doing it. 
+
+let data4: (number | string)[] = [1, 2, "3"] // Now it will have all the mix value that's how you should write the syntax. I think you get it okay.
+
+// also there is one more classic situation:- 
+
+let pi: 3.14 = 3.14 // by this you can assign the direct value its can't be changed...
+
+// her is the more real use case scenario okay . here it is :- 
+// suppose you making a seat booking site for aeroplane 
+
+// here is the example
+
+let seatAllotment: "aisle" | "middle" | "window"; // by writing this you can only access or write this 3 value. You can't over write it .
+
+seatAllotment = "aisle";
+// seatAllotment = "crew" // You can't do that because the crew sit is only for he crew not for general person. just kidding. You cannot access this because the crew doesn't exist inside the seatAllotment
