ce6f79aa52365b43910c4fce1d89a22b62904c30
FerdouseOfficial <mollajihad402@gmail.com>
2025-11-03 17:03:54 +0600

    when copying the code I have also copied the comments of that previous Interface file so I have deleted all the comment because we don't need it nwo

--- Chapter 13/src/InterfaceVSTypes.ts
+++ Chapter 13/src/InterfaceVSTypes.ts
@@ -5,23 +5,17 @@ interface user {
     email?: string,
     id: number,
     googleId?: string
-    // defining a method:-
-    // startTrial: () => string 
-    // anotehr way of doing int the defination of method which hitesh like is this.
-    startTrial(): string // its also a way you can do that
+    startTrial(): string 
     getCoupon(couponName: string, value: number): number
 }
 
-// let hitesh= {name:"hitesh", id: 1}  // that's most beginer make mistake like me they forgot to assign the user interface 
-
-/// here is how you do that
 
 let hitesh: user = {dbId: 22, name: "Hc", id: 1, 
     startTrial : () => {
-        return "Fuck Off Man" // here is how you do it man declare a method
+        return "Fuck Off Man" 
     }, getCoupon:(name: "hitesh10", value: 3) => {
         return 3
     }
-} // that's the perfect way of doing it.
+}
 
 hitesh.email = "WakaWakkaOOOO"
