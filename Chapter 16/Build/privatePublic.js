141f8282cca7db1614c6654ce5ae3d74a5fffe04
FerdouseOfficial <mollajihad402@gmail.com>
2025-11-03 19:25:04 +0600

    finish of this

--- Chapter 16/Build/privatePublic.js
+++ Chapter 16/Build/privatePublic.js
@@ -1,2 +1,26 @@
 "use strict";
-// Learning private public
+// Learning private public 
+// one of the best way of learn to code by seeing reading other code. its not about logic. Its about syntax shortcuts and file structure. 
+// class User{
+//     email: string
+//     name: string
+//     private readonly city: string = "Jaipur"
+//     constructor(email: string, name: string) {
+//         this.email = email;
+//         this.name = name;
+//     }
+// }
+// here is the professional way of writing code. How the professional wriet code
+class User {
+    email;
+    name;
+    city = "Jaipur";
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
+    }
+}
+// this is the shortcut way of doing it.......
+const hitesh = new User("h@ghs", "hitesh");
+// hitesh.city = "delhi"
+// we have learned TypeScript PUBLIC AND PRIVATE:- 
--- Chapter 16/src/privatePublic.ts
+++ Chapter 16/src/privatePublic.ts
@@ -1,1 +1,31 @@
-// Learning private public
+// Learning private public 
+
+// one of the best way of learn to code by seeing reading other code. its not about logic. Its about syntax shortcuts and file structure. 
+
+// class User{
+//     email: string
+//     name: string
+//     private readonly city: string = "Jaipur"
+//     constructor(email: string, name: string) {
+//         this.email = email;
+//         this.name = name;
+//     }
+// }
+
+// here is the professional way of writing code. How the professional wriet code
+
+class User{
+    private readonly city: string = "Jaipur"
+    constructor(
+        public email: string,
+        public name: string) {
+    }
+} 
+
+// this is the shortcut way of doing it.......
+
+
+const hitesh = new User("h@ghs", "hitesh")
+// hitesh.city = "delhi"
+
+// we have learned TypeScript PUBLIC AND PRIVATE:- 
