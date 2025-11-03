79dbdc6c7f2410965488c89c049895353d0f456a
FerdouseOfficial <mollajihad402@gmail.com>
2025-11-03 16:37:28 +0600

    wriiting important comment make sure to read them ut

--- Chapter 12/src/Interfaces.ts
+++ Chapter 12/src/Interfaces.ts
@@ -1,10 +1,19 @@
 // Practicing the crucial concepts of TS is Interfaces. Just like that okay.
 
 interface user {
+    readonly dbId:number
     name: string,
-    id: number
+    email?: string,
+    id: number,
+    googleId?: string
 }
 
 // let hitesh= {name:"hitesh", id: 1}  // that's most beginer make mistake like me they forgot to assign the user interface 
 
 /// here is how you do that
+
+let hitesh: user = {dbId: 22, name: "Hc", id: 1} // that's the perfect way of doing it.
+
+hitesh.email = "WakaWakkaOOOO"
+
+// hitesh.dbId = 34 // we can do that because we cannot change or reassing readonly value remember it
