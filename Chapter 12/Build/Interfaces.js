ca095965b4b0ad49a0777a8d5069734017549cd4
FerdouseOfficial <mollajihad402@gmail.com>
2025-11-03 16:33:18 +0600

    here how we should do that

--- Chapter 12/Build/Interfaces.js
+++ Chapter 12/Build/Interfaces.js
@@ -1,1 +1,4 @@
 "use strict";
+// Practicing the crucial concepts of TS is Interfaces. Just like that okay.
+// let hitesh= {name:"hitesh", id: 1}  // that's most beginer make mistake like me they forgot to assign the user interface 
+/// here is how you do that
---
+++ Chapter 12/src/Interfaces.ts
@@ -0,0 +1,10 @@
+// Practicing the crucial concepts of TS is Interfaces. Just like that okay.
+
+interface user {
+    name: string,
+    id: number
+}
+
+// let hitesh= {name:"hitesh", id: 1}  // that's most beginer make mistake like me they forgot to assign the user interface 
+
+/// here is how you do that
