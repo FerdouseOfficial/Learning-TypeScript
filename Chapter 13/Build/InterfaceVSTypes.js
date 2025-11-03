191ab6ae9d417f189d8342530696c392aee91c93
FerdouseOfficial <mollajihad402@gmail.com>
2025-11-03 17:01:01 +0600

    pasting the previous files code to just practice

--- Chapter 13/Build/InterfaceVSTypes.js
+++ Chapter 13/Build/InterfaceVSTypes.js
@@ -1,2 +1,11 @@
 "use strict";
-// Practicing the concept of Interfaces ad Types okay.....................
+// let hitesh= {name:"hitesh", id: 1}  // that's most beginer make mistake like me they forgot to assign the user interface 
+/// here is how you do that
+let hitesh = { dbId: 22, name: "Hc", id: 1,
+    startTrial: () => {
+        return "Fuck Off Man"; // here is how you do it man declare a method
+    }, getCoupon: (name, value) => {
+        return 3;
+    }
+}; // that's the perfect way of doing it.
+hitesh.email = "WakaWakkaOOOO";
--- Chapter 13/src/InterfaceVSTypes.ts
+++ Chapter 13/src/InterfaceVSTypes.ts
@@ -1,1 +1,27 @@
 // Practicing the concept of Interfaces ad Types okay.....................
+interface user {
+    readonly dbId:number
+    name: string,
+    email?: string,
+    id: number,
+    googleId?: string
+    // defining a method:-
+    // startTrial: () => string 
+    // anotehr way of doing int the defination of method which hitesh like is this.
+    startTrial(): string // its also a way you can do that
+    getCoupon(couponName: string, value: number): number
+}
+
+// let hitesh= {name:"hitesh", id: 1}  // that's most beginer make mistake like me they forgot to assign the user interface 
+
+/// here is how you do that
+
+let hitesh: user = {dbId: 22, name: "Hc", id: 1, 
+    startTrial : () => {
+        return "Fuck Off Man" // here is how you do it man declare a method
+    }, getCoupon:(name: "hitesh10", value: 3) => {
+        return 3
+    }
+} // that's the perfect way of doing it.
+
+hitesh.email = "WakaWakkaOOOO"
