8f1367d51b8ac3b6c6cbfac638b10ac6ed95dd80
FerdouseOfficial <mollajihad402@gmail.com>
2025-11-04 12:18:31 +0600

    make sure to read the code revise it and also read the comment okay. And this is hte finish of this chapter

--- Chapter 19/dist/WhyInterfaceisImportant.js
+++ Chapter 19/dist/WhyInterfaceisImportant.js
@@ -26,3 +26,4 @@ class YouTube {
 }
 // very imporatant notes are here 
 // 1. Alway remember in constructor by following the rule of the interface Yes You have to put property according to the interface if it has 4. You have to add 4. But If you want you can add more properties to that. But you can't add less okay.
+// this is how to do: -------- That's how typeScript generate less error and having more consistency
--- Chapter 19/src/WhyInterfaceisImportant.ts
+++ Chapter 19/src/WhyInterfaceisImportant.ts
@@ -32,3 +32,5 @@ class YouTube implements TakePhoto, Story {
 // very imporatant notes are here 
 
 // 1. Alway remember in constructor by following the rule of the interface Yes You have to put property according to the interface if it has 4. You have to add 4. But If you want you can add more properties to that. But you can't add less okay.
+
+// this is how to do: -------- That's how typeScript generate less error and having more consistency
