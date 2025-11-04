7e8468e0cd25db49f7dd5bd4a548abdd71eb9601
FerdouseOfficial <mollajihad402@gmail.com>
2025-11-04 12:14:30 +0600

    make sure to read the comments okay

--- Chapter 19/dist/WhyInterfaceisImportant.js
+++ Chapter 19/dist/WhyInterfaceisImportant.js
@@ -21,5 +21,5 @@ class YouTube {
         this.shorts = shorts;
     }
 }
-// very imporatant notes are here
+// very imporatant notes are here 
 // 1. Alway remember in constructor by following the rule of the interface Yes You have to put property according to the interface if it has 4. You have to add 4. But If you want you can add more properties to that. But you can't add less okay.
