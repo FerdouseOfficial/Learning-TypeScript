2d304d15bf5d963199b9e7fcad09db98dee18553
FerdouseOfficial <mollajihad402@gmail.com>
2025-11-04 11:55:54 +0600

    there is nothing left in this chapter okay. Make sure to read the comments okay.

--- chapter 18/dist/Protect.js
+++ chapter 18/dist/Protect.js
@@ -26,5 +26,14 @@ class User {
         this._courseCount = courseNum;
     }
 }
+class subUser extends User {
+    isFamily = true;
+    changeCourseCount() {
+        this._courseCount = 4; // this is not possible  
+    }
+}
 // we can use Inheritace to create subUser....
 // but we can't access the private value by that. As we already know that by Using Private Keyword we can only access that withing the class. Just like that 
+// Protected keyword in TypeScript allows access within the same class and any classes that inherit from it.
+// Unlike private which is only accessible within the declaring class.
+// It's useful when you want to share class members with child classes but keep them hidden from external code. That's it I hope you understand this 
