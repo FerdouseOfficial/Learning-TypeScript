2442b797c1397c5e6a997caecbbe598ce56b9dcd
FerdouseOfficial <mollajihad402@gmail.com>
2025-11-04 11:47:45 +0600

    i have copy paste the code from the previous chapter just to practice the Protech in this same code base

--- chapter 18/dist/Protect.js
+++ chapter 18/dist/Protect.js
@@ -1,1 +1,28 @@
 "use strict";
+class User {
+    email;
+    name;
+    _courseCount = 1;
+    city = "Jaipur";
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
+    }
+    get getAppleEmail() {
+        return `apple ${this.email}`;
+    }
+    // Now If you wanna read or access the private courseCount here is how you do it.
+    get courseCount() {
+        return this._courseCount;
+    }
+    // here is how you set value inside it........
+    // one more imporatnt things about setter. 
+    // You are not able set types of setter okay. You can set return or type in setter okay remember it. 
+    // there is no return type in setter in TypeScript
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("course count should be more than 1");
+        }
+        this._courseCount = courseNum;
+    }
+}
