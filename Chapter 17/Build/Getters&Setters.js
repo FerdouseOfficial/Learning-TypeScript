2cf0a75f40938fb5fab422799a61d221f5968bdb
FerdouseOfficial <mollajihad402@gmail.com>
2025-11-04 11:40:50 +0600

    forgot to setupt the file 18 to practice

--- Chapter 17/Build/Getters&Setters.js
+++ Chapter 17/Build/Getters&Setters.js
@@ -15,14 +15,14 @@ body.style.backgroundColor = "#000";
 // here is the professional way of writing code. How the professional wriet code
 // class User{
 //     private _courseCount = 1;  
-//     private readonly city: string = "Jaipur"
-//     constructor(
-//         public email: string,
-//         public name: string) {
-//     }
-//     get getAppleEmail(): string {
-//         return `apple ${this.email}`;
-//     }
+// private readonly city: string = "Jaipur"
+// constructor(
+//     public email: string,
+//     public name: string) {
+// }
+// get getAppleEmail(): string {
+//     return `apple ${this.email}`;
+// }
 // } 
 // this is the shortcut way of doing it.......
 // const hitesh = new User("h@ghs", "hitesh")
@@ -66,6 +66,29 @@ body.style.backgroundColor = "#000";
 // let b = person.fullName = "Joe Rogan"
 // console.log(b)
 class User {
+    email;
+    name;
     _courseCount = 1;
-    city = "Japipur";
+    city = "Jaipur";
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
+    }
+    get getAppleEmail() {
+        return `apple ${this.email}`;
+    }
+    // Now If you wanna read or access the private courseCount here is how you do it.
+    get courseCount() {
+        return this._courseCount;
+    }
+    // here is how you set value inside it........
+    // one more imporatnt things about setter. 
+    // You are not able set types of setter okay. You can set return or type in setter okay remember it. 
+    // there is no return type in setter in TypeScript
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("course count should be more than 1");
+        }
+        this._courseCount = courseNum;
+    }
 }
--- Chapter 17/src/Getters&Setters.ts
+++ Chapter 17/src/Getters&Setters.ts
@@ -19,14 +19,14 @@ body.style.backgroundColor = "#000"
 // class User{
 //     private _courseCount = 1;  
 
-//     private readonly city: string = "Jaipur"
-//     constructor(
-//         public email: string,
-//         public name: string) {
-//     }
-//     get getAppleEmail(): string {
-//         return `apple ${this.email}`;
-//     }
+    // private readonly city: string = "Jaipur"
+    // constructor(
+    //     public email: string,
+    //     public name: string) {
+    // }
+    // get getAppleEmail(): string {
+    //     return `apple ${this.email}`;
+    // }
 // } 
 
 
@@ -96,5 +96,34 @@ body.style.backgroundColor = "#000"
 class User {
     private _courseCount = 1;
 
-    readonly city: string = "Japipur"
+    private readonly city: string = "Jaipur"
+    constructor(
+        public email: string,
+        public name: string) {
+    }
+    get getAppleEmail(): string {
+        return `apple ${this.email}`;
+    }
+
+    // Now If you wanna read or access the private courseCount here is how you do it.
+
+    get courseCount(): number {
+        return this._courseCount
+    }
+
+    // here is how you set value inside it........
+     // one more imporatnt things about setter. 
+
+
+
+     // You are not able set types of setter okay. You can set return or type in setter okay remember it. 
+
+     // there is no return type in setter in TypeScript
+
+     set courseCount(courseNum) {
+        if(courseNum <= 1) {
+            throw new Error("course count should be more than 1")
+        }
+        this._courseCount = courseNum 
+     }
 }
