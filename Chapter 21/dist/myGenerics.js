47c126a8beff96bfa40613ceb8fc5896e508f78b
FerdouseOfficial <mollajihad402@gmail.com>
2025-11-04 15:54:24 +0600

    writing actuall generics and wriitig very important comment you must read them

--- Chapter 21/dist/myGenerics.js
+++ Chapter 21/dist/myGenerics.js
@@ -2,3 +2,15 @@
 const score = [];
 const names = [];
 // this is generics 
+function ntityOne(val) {
+    return val;
+}
+function identityTwo(val) {
+    return val;
+}
+// the actual way of writing generics here is how you write it :-
+function identityThree(val) {
+    return val;
+}
+identityThree(3); // let me make it clear how it works actually when You use <> I mean Generics  its automatically tract the type of the value. and set it up as a type of the function also for the val parameter also fo the function return type just like that. 
+// If you want to clearly understand just mouseOver in the identityThree(3) then you will see how it works
--- Chapter 21/src/myGenerics.ts
+++ Chapter 21/src/myGenerics.ts
@@ -2,3 +2,21 @@ const score: Array<number> = []
 const names: Array<string> = []
 
 // this is generics 
+
+function ntityOne(val: number | boolean):  number | boolean {
+    return val
+}
+
+function identityTwo(val: any): any {
+    return val;
+}
+
+// the actual way of writing generics here is how you write it :-
+
+function identityThree<Type>(val: Type): Type {
+    return val
+}
+
+identityThree(3) // let me make it clear how it works actually when You use <> I mean Generics  its automatically tract the type of the value. and set it up as a type of the function also for the val parameter also fo the function return type just like that. 
+
+// If you want to clearly understand just mouseOver in the identityThree(3) then you will see how it works
