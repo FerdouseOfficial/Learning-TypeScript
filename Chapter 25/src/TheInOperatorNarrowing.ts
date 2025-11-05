4d1604f5cb8d3e01b7241102becb7d79e9c40a60
FerdouseOfficial <mollajihad402@gmail.com>
2025-11-05 22:08:26 +0600

    finish of this chapter

--- Chapter 25/src/TheInOperatorNarrowing.ts
+++ Chapter 25/src/TheInOperatorNarrowing.ts
@@ -15,4 +15,4 @@ let isAdmin = (account: User | Admin) => {
     if("isAdmin" in account){
         return account.isAdmin // I have some confusion here I will write it later if I forgot that I will come back again or see this chunck of tutorial and find out what I wanna write okay
     }
-}
+} 
