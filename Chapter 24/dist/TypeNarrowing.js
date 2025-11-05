438c04ee419de349c0cd5211c490fa56e7ed7759
FerdouseOfficial <mollajihad402@gmail.com>
2025-11-05 21:35:11 +0600

    writing imp comments make sure to check it out this comment is much �

--- Chapter 24/dist/TypeNarrowing.js
+++ Chapter 24/dist/TypeNarrowing.js
@@ -1,1 +1,4 @@
 "use strict";
+// Practicing Type Narrowing here okay !
+// don't just stick with what the tutorial teach you.
+// make sure. I mean obviously You must have to how could I say You need this ASS to be done you have to read the documentation okay.
