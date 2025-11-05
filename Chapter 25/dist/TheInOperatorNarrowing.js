94f40b27c3a49ddcb1359338c1337f3e7612fe61
FerdouseOfficial <mollajihad402@gmail.com>
2025-11-05 22:07:47 +0600

    writing the actual code and some imp comment with it make sure to check it out okay

--- Chapter 25/dist/TheInOperatorNarrowing.js
+++ Chapter 25/dist/TheInOperatorNarrowing.js
@@ -1,2 +1,7 @@
 "use strict";
 // in operator
+let isAdmin = (account) => {
+    if ("isAdmin" in account) {
+        return account.isAdmin; // I have some confusion here I will write it later if I forgot that I will come back again or see this chunck of tutorial and find out what I wanna write okay
+    }
+};
--- Chapter 25/src/TheInOperatorNarrowing.ts
+++ Chapter 25/src/TheInOperatorNarrowing.ts
@@ -1,1 +1,18 @@
 // in operator
+
+interface User{
+    name: string,
+    email: string
+}
+
+interface Admin{
+    name:string,
+    email: string,
+    isAdmin: boolean
+}
+
+let isAdmin = (account: User | Admin) => {
+    if("isAdmin" in account){
+        return account.isAdmin // I have some confusion here I will write it later if I forgot that I will come back again or see this chunck of tutorial and find out what I wanna write okay
+    }
+}
