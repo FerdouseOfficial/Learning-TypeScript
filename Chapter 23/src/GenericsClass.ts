d113512e0ce5de64a7f1fdf35d389ba602bc5387
FerdouseOfficial <mollajihad402@gmail.com>
2025-11-05 18:23:14 +0600

    writing important commment inside the ts file

---
+++ Chapter 23/src/GenericsClass.ts
@@ -0,0 +1,19 @@
+// Generics class practice okay:--
+
+// | Concept                                  | Meaning                                     |
+// | ---------------------------------------- | ------------------------------------------- |
+// | **Generic (`T`)**                        | A placeholder for any type                  |
+// | **Generic Constraint (`T extends ...`)** | A rule that says **T must have some shape** |
+
+
+// 🎯 Easiest Real-life Example
+
+// Imagine you say:
+
+// Anyone can enter the classroom. (Generic)
+
+// But then you add a rule:
+
+// Only students wearing ID cards can enter. (Generic Constraint)
+
+// So the ID card requirement = extends.
