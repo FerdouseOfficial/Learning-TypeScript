5a51410ec491d8be2a83303b2d5893029b3cfae1
FerdouseOfficial <mollajihad402@gmail.com>
2025-11-05 22:12:58 +0600

    follow the instruction provide in comments okay

--- Chapter 26/dist/Instaceof and Type Predicates.js
+++ Chapter 26/dist/Instaceof and Type Predicates.js
@@ -1,1 +1,3 @@
 "use strict";
+// Start Practcing hte concept of Instanceof and Type Predicts
+// I don't understand what the code actually mean ask GPT okay. and learn from him 
---
+++ Chapter 26/src/Instaceof and Type Predicates.ts
@@ -0,0 +1,3 @@
+// Start Practcing hte concept of Instanceof and Type Predicts
+
+// I don't understand what the code actually mean ask GPT okay. and learn from him 
