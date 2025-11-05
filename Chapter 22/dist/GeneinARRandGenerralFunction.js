e1581f4bbd11606ea8f4fcbf5cadc4db980313b1
FerdouseOfficial <mollajihad402@gmail.com>
2025-11-05 18:05:56 +0600

    the chapter 22 is finished

--- Chapter 22/dist/GeneinARRandGenerralFunction.js
+++ Chapter 22/dist/GeneinARRandGenerralFunction.js
@@ -1,1 +1,5 @@
 "use strict";
+function getSearchProducts(products) {
+    return products[3];
+}
+getSearchProducts([1, 2, 3]);
---
+++ Chapter 22/src/GeneinARRandGenerralFunction.ts
@@ -0,0 +1,24 @@
+
+interface bottle{
+    brand: string,
+    type: number
+}
+
+function getSearchProducts<T>(products: T[]): T {
+
+    // here  is more precise way of doing things okay..............
+
+    // do some database operations
+    const myIndex = 1;
+    // return products[myIndex]
+
+    return products[myIndex]; // here I have face some issue with some incorrect syntax like this return products[] this; product[3] this because the index 3 there in no value in the argument of getSerchProducts its just have 3 value 1, 2, 3, so there is no value in index 3 that's why its showing us error okay .
+}
+
+// getSearchProducts([1, 2, 3])
+
+const getMoreSearchData = <T>(products: T[]): T => { // remember in many code base you will see <T,> this okay no need to become confuse this is okay. People who use react by using the comman they make sure that this is not a JSX syntax. This is a generics type okay.
+    // do some database operations..... 
+    const myIndex = 4;
+    return products[myIndex];
+}
