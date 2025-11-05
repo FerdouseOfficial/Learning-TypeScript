5d6e664ea4f68d17e693016d21fb9e96afdc8548
FerdouseOfficial <mollajihad402@gmail.com>
2025-11-05 19:24:04 +0600

    writing very important code which I have already wrote in Chapter 21. and make sure to read the comment and learn what is the problem

--- Chapter 23/dist/GenericsClass.js
+++ Chapter 23/dist/GenericsClass.js
@@ -10,3 +10,42 @@
 // But then you add a rule:
 // Only students wearing ID cards can enter. (Generic Constraint)
 // So the ID card requirement = extends.
+// ⭐ Summary:-
+// Generics allow flexibility.
+// Generic Constraints allow controlled flexibility.
+// They ensure that you can only pass types that match certain rules.
+const score = [];
+const Name = [];
+function identityOne(val) {
+    return val;
+}
+function indentityTwo(val) {
+    return val;
+}
+function identityThree(val) {
+    return val;
+}
+function identityFour(val) {
+    return val;
+}
+//identityFour<Bottle> ({})
+// function getSearchProducts<T>(products: T): T{
+//     //do some database operations
+//     const myIndex = 2;
+//     // return products[myIndex] // Now I am just don't write this because its throwing erro on me so I have ignore it for now oka.
+//     return products
+// }
+// const getMoreSearchProducts<T,>(products: T): T { // reserach what is the problem here okay. then figure it out
+//     return products
+// }
+// I have face this Issue before And I have chat solution about this with chatGPT make sure to read and check it out okay. bsource desktop account 
+function getSearchProducts(products) {
+    //do some database operations
+    const myIndex = 2;
+    // return products[myIndex] // Now I am just don't write this because its throwing erro on me so I have ignore it for now oka.
+    return products;
+}
+const getMoreSearchProducts;
+(products) => {
+    return products;
+};
--- Chapter 23/src/GenericsClass.ts
+++ Chapter 23/src/GenericsClass.ts
@@ -17,3 +17,63 @@
 // Only students wearing ID cards can enter. (Generic Constraint)
 
 // So the ID card requirement = extends.
+
+
+// ⭐ Summary:-
+
+// Generics allow flexibility.
+
+// Generic Constraints allow controlled flexibility.
+
+// They ensure that you can only pass types that match certain rules.
+
+
+
+const score: Array<number> = []
+const Name: Array<string> = []
+
+function identityOne(val: boolean | number): boolean | number{
+    return val
+}
+
+function indentityTwo(val: any): any {
+    return val;
+}
+function identityThree<Type>(val: Type): Type {
+    return val
+}
+
+function identityFour<T>(val: T): T {
+    return val;
+}
+
+interface Bottle{
+    brand: string,
+    type: number
+}
+
+//identityFour<Bottle> ({})
+
+// function getSearchProducts<T>(products: T): T{
+//     //do some database operations
+//     const myIndex = 2;
+//     // return products[myIndex] // Now I am just don't write this because its throwing erro on me so I have ignore it for now oka.
+//     return products
+// }
+
+// const getMoreSearchProducts<T,>(products: T): T { // reserach what is the problem here okay. then figure it out
+//     return products
+// }
+
+// I have face this Issue before And I have chat solution about this with chatGPT make sure to read and check it out okay. bsource desktop account 
+
+function getSearchProducts<T>(products: T[]): T{
+    //do some database operations
+    const myIndex = 2;
+    // return products[myIndex] // Now I am just don't write this because its throwing erro on me so I have ignore it for now oka.
+    return products
+}
+
+const getMoreSearchProducts<T,>(products: T[]): T { // reserach what is the problem here okay. then figure it out
+    return products
+}
