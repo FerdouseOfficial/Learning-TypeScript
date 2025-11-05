2a5e3bad7eeaecd17889bca21c5f29031ea0325f
FerdouseOfficial <mollajihad402@gmail.com>
2025-11-05 18:06:46 +0600

    the finish of chapter 22

--- Chapter 22/dist/GeneinARRandGenerralFunction.js
+++ Chapter 22/dist/GeneinARRandGenerralFunction.js
@@ -1,5 +1,14 @@
 "use strict";
 function getSearchProducts(products) {
-    return products[3];
+    // here  is more precise way of doing things okay..............
+    // do some database operations
+    const myIndex = 1;
+    // return products[myIndex]
+    return products[myIndex]; // here I have face some issue with some incorrect syntax like this return products[] this; product[3] this because the index 3 there in no value in the argument of getSerchProducts its just have 3 value 1, 2, 3, so there is no value in index 3 that's why its showing us error okay .
 }
-getSearchProducts([1, 2, 3]);
+// getSearchProducts([1, 2, 3])
+const getMoreSearchData = (products) => {
+    // do some database operations..... 
+    const myIndex = 4;
+    return products[myIndex];
+};
