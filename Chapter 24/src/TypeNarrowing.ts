f984accc9c2a668a3937146c364d52bbab33caaa
FerdouseOfficial <mollajihad402@gmail.com>
2025-11-05 21:46:55 +0600

    writing code and impo comments

--- Chapter 24/src/TypeNarrowing.ts
+++ Chapter 24/src/TypeNarrowing.ts
@@ -3,3 +3,18 @@
 // don't just stick with what the tutorial teach you.
 
 // make sure. I mean obviously You must have to how could I say You need this ASS to be done you have to read the documentation okay.
+
+// Hitesh name is practice file as Detaction.ts okay
+
+// remember you have to be very cautious in writing the business case okay. Remember that
+
+function detectType(val: number | string){
+    if(typeof val === "string"){
+        return val.toLowerCase()
+    }
+    return val + 3
+}
+
+// this all are cautionary tales so make sure ou pay attention on the caution not what could be the solution ther. 
+
+// If you have issue with understanding anything here the code and the comment anything just go to the times of of TypeNarrowing in TS okay you will undersand that. 
